6b35cbe708684ce385a3a5ffae022621dcc7be5f
salmanpatnee <salmanpatni92@gmail.com>
2025-02-28 00:02:37 +0500

    Set up Streamlit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from growth-mindset-challenge!")
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
 
+# Set up app
+st.set_page_config(page_title="Data sweeper", layout="wide")
+st.title("Data sweeper")
+st.write("Transform your files between CSV and Excel formats.")
 
-if __name__ == "__main__":
-    main()
+st.file_uploader("Upload your files (CSV or Excel):", type=["csv", "xlsx"], accept_multiple_files=True)
