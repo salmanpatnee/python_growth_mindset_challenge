c15976052e027d64f738737ae9474c72c75b5f21
salmanpatnee <salmanpatni92@gmail.com>
2025-02-28 23:58:10 +0500

    Add data cleaning options

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if uploaded_files:
 
         if file_ext == ".csv":
             df = pd.read_csv(file)
-        elif file_ext == "xlsx":
+        elif file_ext == ".xlsx":
             df = pd.read_excel(file)
         else:
             st.error(f"Unsupported file type: {file_ext}")
@@ -25,3 +25,27 @@ if uploaded_files:
 
         # Display file info
         st.write(f"**File Name:** {file.name}")
+
+        st.write("Preview")
+        st.dataframe(df.head())
+
+        st.subheader("Data cleaning Options")
+        if st.checkbox(f"Clean data for file {file.name}"):
+            col1, col2 = st.columns(2)
+
+            with col1:
+                if st.button(f"Remove duplicates from {file.name}"):
+                    df.drop_duplicates(inplace=True)
+                    st.write("Duplicates Removed!")
+
+            with col2:
+                if st.button(f"Fill missing values for {file.name}"):
+                    numeric_cols = df.select_dtypes(include=['number']).columns
+                    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+                    st.write("Missing values have been Filled!")
+
+
+        st.write("Select columns to convert.")
+        columns = st.multiselect(f"Choose columns for {file.name}", df.columns, default=df.columns)
+        df = df[columns]
+
